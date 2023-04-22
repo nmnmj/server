3b89085355553b21ac53a853151e4e29fb73e122
nmnmj <nmnjay@gmail.com>
2023-04-23 01:37:09 +0530

    f

--- routes/web.js
+++ routes/web.js
@@ -3,6 +3,9 @@ import Paymentcontroller from '../controller/Paymentcontroller.js'
 
 const router = express.Router()
 
+router.get("/",(req, res)=>{
+    res.send("work")
+})
 router.post("/checkout", Paymentcontroller.checkout)
 router.post("/verification", Paymentcontroller.paymentverification)
 
